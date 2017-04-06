951b13a165f40459aa67dbcb636060f997f40e30
Glen Langer <contao.modul@glen-langer.de>
2017-04-06 23:02:11 +0200

    PHPDoc korrigiert

--- src/BugBusterBotdetectionBundle.php
+++ src/BugBusterBotdetectionBundle.php
@@ -3,7 +3,7 @@
 /**
  * @copyright  Glen Langer 2017 <http://contao.ninja>
  * @author     Glen Langer (BugBuster)
- * @package    Xing
+ * @package    BotDetection
  * @license    LGPL-3.0+
  * @see	       https://github.com/BugBuster1701/contao-botdetection-bundle
  *
