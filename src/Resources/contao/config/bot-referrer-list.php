184cf4741376d06152e3edb99187508d80490f9c
Glen Langer <contao.modul@glen-langer.de>
2018-10-20 23:57:33 +0200

    Fixed #17,#18,#19,#20,#21 New Bots and Referrer Spam

--- src/Resources/contao/config/bot-referrer-list.php
+++ src/Resources/contao/config/bot-referrer-list.php
@@ -11,25 +11,37 @@ $botreferrerlist = array(
 'creativebiz.de',  // #120
 'cylex.de',  // #init
 'darkhalfe.ru',  // #init
+'docsportal.net', // #17 C44
 'DirectI.com',  // #120
+'eduinfosite.com', // #17 C44
 'extener.org',  // #init
 'fanyi.baidu.com',  // #120
 'fastwebserver.de', // #142
+'filesdatabase.net', // #17 C44
 'fx-ray.com',  // #120
 'hosted-by.leaseweb.com', // #141
+'hoztorg-opt.ru', // #17 C44
 'japfm.com',  // #init
+'kinosed.net', // #17 C44
+'laminat.com.ua', // #17 C44
+'metallo-konstruktsii.ru',
 'newworldmayfair.com',  // #init
+'office2web.com', // #17 C44
 'openfrost.net',  // #init
 'openmediasoft.com',  // #init
 'plaghunter.com',  // #120
+'pornonik.com', // #17 C44
 'resellerclubs cam',  // #120
 'sovetogorod.ru',  // #init
+'spb-scenar.ru', // #18 C44
 'specialdessous.com',  // #init
+'tattoo-stickers.ru', // #17 C44
 'updown_tester',  // #init
 'VitalityrulesGoogle',  // #120
 'vitaly rules google',  // #120
 'weesexy.ru',  // #init
 'wpsecuritycheck.co.uk',  // #120
 'wpthemedetector.co.uk',  // #120
+'www.regionshop.biz', // #17 C44
 'www.xxlpromo.com'  // #init
 );
