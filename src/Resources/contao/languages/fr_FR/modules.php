50c964acc354aa9116fae0e509cd7d075bd52f55
Glen Langer <contao.modul@glen-langer.de>
2018-11-09 23:27:54 +0100

    New translation: fr_FR

---
+++ src/Resources/contao/languages/fr_FR/modules.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-botdetection-bundle/language/fr_FR/
+ *
+ * last-updated: 2018-11-09T23:21:03+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['FMD']['botdetection1']['0'] = 'Bot Detection Demo 1';
+$GLOBALS['TL_LANG']['FMD']['botdetection1']['1'] = 'Demo 1 pour l\'utilisation de Bot Detection avec import.';
+$GLOBALS['TL_LANG']['FMD']['botdetection2']['0'] = 'Bot Detection Demo 2';
+$GLOBALS['TL_LANG']['FMD']['botdetection2']['1'] = 'Demo 2 pour Bot Detection (Formulaire).';
+$GLOBALS['TL_LANG']['MOD']['botdetection']['0']  = 'Détection Bot';
+$GLOBALS['TL_LANG']['MOD']['botdetection']['1']  = 'Helper class qui détecte les moteurs de recherche, les robots, les robots d\'indexation de pages web, les robots collecteur';
+
