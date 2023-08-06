4dbc6a5e0e5e89818606ff90855c134dd0c5f133
Glen Langer <contao.modul@glen-langer.de>
2023-08-06 20:47:49 +0200

    BOTDETECTION_VERSION auf 1.8.1

--- src/Resources/contao/modules/ModuleBotDetection.php
+++ src/Resources/contao/modules/ModuleBotDetection.php
@@ -34,7 +34,7 @@ class ModuleBotDetection extends System
     /**
      * Current version of the class.
      */
-    const BOTDETECTION_VERSION  = '1.8.0';
+    const BOTDETECTION_VERSION  = '1.8.1';
 
     const BOT_REFERRER_LIST     = "/vendor/bugbuster/contao-botdetection-bundle/src/Resources/contao/config/bot-referrer-list.php";
     const BOT_REFERRER_PROVIDER = "/vendor/bugbuster/contao-botdetection-bundle/src/Resources/contao/config/referrer-provider.php";
